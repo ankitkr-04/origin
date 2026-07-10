8b444985d780be803a9ba54c55b7b2df51a8204b
Ankit Kumar <ak0182274@gmail.com>
2026-07-10 10:05:06 +0530

    chore: format next.config.ts with biome

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  experimental:{
+  experimental: {
     viewTransition: true,
     webVitalsAttribution: ["CLS", "FCP", "FID", "LCP", "TTFB"],
-  }
+  },
 };
 
 export default nextConfig;
